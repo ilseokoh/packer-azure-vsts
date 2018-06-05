af04665b37bb419dd11523b18b59d72f8b158dd0
오일석 <onestone@live.co.kr>
2018-06-05 16:08:44 +0900

    Add shell-local

--- build-baseimage.ps1
+++ build-baseimage.ps1
@@ -1,17 +1,17 @@
-Param(
-    [string]$latestimagename
-)
+# Get image name from text file
+$latestimagename = Get-Content .\latest-imagename.txt -Raw
+$latestimagename = $latestimagename.Trim()
 
-# & "packer.exe validate -var-file=variable.json -var 'custom_managed_image_name=$latestimagename' .\bdm-baseimage.json"
-#& .\packer.exe build $option
 $cmdPath = "$PSScriptRoot\packer.exe"
 $cmdArgList = @(
 	"build",
+	# "validate",
 	# "-debug",
 	"-var-file=.\variable.json",
 	"-var","custom_managed_image_name=$latestimagename"
 	".\bdm-baseimage.json"
 )
+
 Write-Output "image name: $latestimagename"
 Write-Output "cmd: $cmdPath"
 Write-Output "arg: $cmdArgList"
