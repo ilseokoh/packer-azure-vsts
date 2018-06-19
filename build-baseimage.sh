9576bd285e6f328165da8a842f1079adae0c13b3
오일석 <onestone@live.co.kr>
2018-06-19 17:12:20 +0900

    delete files for bash

--- build-baseimage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "Image Name: %s\n" $1
-
-# Build Image
-./packer build -var-file=./variable.json \
-             -var "custom_managed_image_name=$1" \
-            ./bdm-baseimage.json 
