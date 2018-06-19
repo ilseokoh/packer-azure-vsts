15c0cf5ffd16a089afe8a9bf00a28db234d602af
오일석 <onestone@live.co.kr>
2018-06-19 17:11:32 +0900

    update by tag

--- powershell/update-latest-imagename.ps1
+++ powershell/update-latest-imagename.ps1
@@ -4,4 +4,11 @@ Param(
 
 #update latest-imagename.txt
 Write-Output "New Image name: $newimagename"
-$newimagename | Out-File -FilePath 'latest-imagename.txt' -Force
+
+#remove all tag in the resoruce group 
+$current = (Get-AzureRmResource -ODataQuery "`$filter=tagname eq 'version' and tagvalue eq 'latest'")
+Set-AzureRmResource -Tag @{ } -ResourceId $current.ResourceId -Force
+
+#set tag to the new image  
+$r = Get-AzureRmResource | Where-Object ResourceName -eq $newimagename
+Set-AzureRmResource -Tag @{ version="latest"} -ResourceId $r.ResourceId -Force
